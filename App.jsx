0a4fc6a9e3d4e3a2511dcd6ca66dd91232a311cb
maccevedor <maccevedor@gmail.com>
2018-03-06 20:28:05 -0500

    Finish V4 four

--- App.jsx
+++ App.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 
-class App extends React.Component{
+Resolutions = new Mongo.Collection("resolutions");
 
+
+export default class App extends React.Component{
+
+    addResolution(event){
+        event.preventDefault();
+        var text = this.refs.resolution.value.trim();
+        Resolutions.insert({
+            text:text,
+            complete: false,
+            createdAt: new Date()
+        });
+        this.refs.resolution.value = "";
+    }
     render(){
         return(
-            <h1>Hello World</h1>    
+            <div>
+                <h1>Hello World1</h1>    
+                <form className="new-resolution" onSubmit={this.addResolution.bind(this)} >
+                    <input 
+                        type="text" 
+                        ref="resolution"
+                        placeholder="submit"/>
+                </form>
+            </div>
         )
     }
 }
 
-if(Meteor.isClient){
-    Meteor.startup(function(){
-        React.render(<App />, document.getElementById("render-target"));
-    });
-}
+// if(Meteor.isClient){
+//     Meteor.startup(function(){
+//         React.render(<App />, document.getElementById("render-target"));
+//     });
+// }
