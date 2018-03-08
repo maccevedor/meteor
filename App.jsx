f0d33199cc7d3396591697bb4976717de0e10479
maccevedor <maccevedor@gmail.com>
2018-03-07 21:03:17 -0500

    v7

--- App.jsx
+++ App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import TrackerReact from "meteor/ultimatejs:tracker-react";
+import ResolutionsForm from "./ResolutionsForm.jsx";
+import ResolutionSingle from './ResolutionSingle.jsx'
 Resolutions = new Mongo.Collection("resolutions");
 
 
@@ -10,16 +12,16 @@ export default class App extends TrackerReact(React.Component){
 
     }
 
-    addResolution(event){
-        event.preventDefault();
-        var text = this.refs.resolution.value.trim();
-        Resolutions.insert({
-            text:text,
-            complete: false,
-            createdAt: new Date()
-        });
-        this.refs.resolution.value = "";
-    }
+    // addResolution(event){
+    //     event.preventDefault();
+    //     var text = this.refs.resolution.value.trim();
+    //     Resolutions.insert({
+    //         text:text,
+    //         complete: false,
+    //         createdAt: new Date()
+    //     });
+    //     this.refs.resolution.value = "";
+    // }
     render(){
         let res = this.resolutions();
         if(res.length <1){
@@ -28,16 +30,17 @@ export default class App extends TrackerReact(React.Component){
         console.log(this.resolutions());
         return(
             <div>
-                <h1>Hello World1</h1>    
-                <form className="new-resolution" onSubmit={this.addResolution.bind(this)} >
+                <h1>My Resolutions</h1>    
+                {/* <form className="new-resolution" onSubmit={this.addResolution.bind(this)} >
                     <input 
                         type="text" 
                         ref="resolution"
                         placeholder="submit"/>
-                </form>
-                <div>
-                    {res[0].text}
-                </div>
+                </form> */}
+                <ResolutionsForm />
+                <ul>
+                    <ResolutionSingle resolution= {res[0]} />
+                </ul>
             </div>
         )
     }
