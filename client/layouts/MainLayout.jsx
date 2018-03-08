c262c0b4fbba36bae5b6cc55bfbad010e217d051
maccevedor <maccevedor@gmail.com>
2018-03-07 22:18:10 -0500

    V7

--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-export const MainLayout = ({content}) => (
-    <div className="main-layout">
-        {content}
-    </div>
+export const MainLayout = ({ content }) => (
+ <div className="main-layout">
+           <header>
+             <h2>My resolutions</h2>
+             <nav>
+               <a href="/">Resolutions</a>
+               <a href="/about">About</a>
+             </nav>
+           </header>
+           <main>
+           {content}
+
+           </main>
+         </div>
 )
