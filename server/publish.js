3a327a79b455577e6193e8b5ff90ca45273982cb
maccevedor <maccevedor@gmail.com>
2018-03-09 18:06:39 -0500

    v11

--- server/publish.js
+++ server/publish.js
@@ -1,5 +1,6 @@
 Resolutions = new Mongo.Collection("resolutions");
 
 Meteor.publish("allResolutions",function(){
-    return Resolutions.find({complete:false});
+    //return Resolutions.find({complete:false});
+    return Resolutions.find({});
 });
