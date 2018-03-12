03c8c9f05b39ffe63a7c93ece3a9955e46dd2c26
maccevedor <maccevedor@gmail.com>
2018-03-11 19:05:43 -0500

    v15

--- client/resolutions/ResolutionsForm.jsx
+++ client/resolutions/ResolutionsForm.jsx
@@ -6,9 +6,23 @@ export default class ResolutionsForm extends Component {
     addResolution(event){
     event.preventDefault();
     var text = this.refs.resolution.value.trim();
-    Meteor.call('addResolution',text ,()=> {
-        this.refs.resolution.value = "";
+    if(text){
+        Meteor.call('addResolution',text ,(error,data )=> {
+        if (error) {
+            Bert.alert('please login','danger','fixed-top','fa-frown-o')
+        }else{
+            this.refs.resolution.value = "";        
+        }
+    }
+    // Meteor.call('addResolution',text ,()=> {
+    //     this.refs.resolution.value = "";
+    // });
+
+
+
+
     });
+
 
     }
 
