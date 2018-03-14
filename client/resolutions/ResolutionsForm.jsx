36bc17d935557f70b16263ac6b82a3038d03ca82
maccevedor <maccevedor@gmail.com>
2018-03-13 21:27:03 -0500

    se soluciona el error

--- client/resolutions/ResolutionsForm.jsx
+++ client/resolutions/ResolutionsForm.jsx
@@ -1,40 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class ResolutionsForm extends Component {
-
-
-    addResolution(event){
+  addResolution(event) {
     event.preventDefault();
     var text = this.refs.resolution.value.trim();
-    if(text){
-        Meteor.call('addResolution',text ,(error,data )=> {
+    if (text) {
+      Meteor.call("addResolution", text, (error, data) => {
         if (error) {
-            Bert.alert('please login','danger','fixed-top','fa-frown-o')
-        }else{
-            this.refs.resolution.value = "";        
+          Bert.alert("please login", "danger", "fixed-top", "fa-frown-o");
+        } else {
+          this.refs.resolution.value = "";
         }
+
+        // Meteor.call('addResolution',text ,()=> {
+        //     this.refs.resolution.value = "";
+        // });
+      });
     }
-    // Meteor.call('addResolution',text ,()=> {
-    //     this.refs.resolution.value = "";
-    // });
+  }
 
-
-
-
-    });
-
-
-    }
-
-    render(){
-        return(
-                <form className="new-resolution" onSubmit={this.addResolution.bind(this)} >
-                    <input 
-                        type="text" 
-                        ref="resolution"
-                        placeholder="submit"/>
-                </form>
-        )
-
-    }
+  render() {
+    return (
+      <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
+        <input type="text" ref="resolution" placeholder="submit" />
+      </form>
+    );
+  }
 }
